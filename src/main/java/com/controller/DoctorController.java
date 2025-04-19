628e8f6922ccdf844e62a9046f4874affb4fddbc
Chirag B Jogi <cheetahchirag04@gmail.com>
2025-04-19 11:51:32 +0530

    Update DoctorController.java

--- src/main/java/com/controller/DoctorController.java
+++ src/main/java/com/controller/DoctorController.java
@@ -24,7 +24,6 @@ public class DoctorController {
 	@Autowired
 	private PatientService patientService;
 	
-//Doctor Functionalities Start ---------------------------------------------------------------------------------------------------------------------
 	
 	@RequestMapping("/viewPatientEnterId")
 	public ModelAndView viewPatientEnterIdController() {
